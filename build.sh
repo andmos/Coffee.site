2ee3e385d0e34e4e3a09f7c663205273f52ac699
andmos <andreas.mosti@gmail.com>
2016-12-12 20:57:55 +0100

    build the site too

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ if [ ! -d "docs/" ]; then
     git clone https://github.com/andmos/coffee docs
     mv docs/README.md docs/index.md
 else 
-    cd docs/ && git pull
+    cd docs/ && git pull && cd ..
 fi
+mkdocs build
